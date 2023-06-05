899bf5255a04bfb72453d658c173fb6646609106
Manav <manavt2000@gmail.com>
2023-06-05 23:07:56 +0200

    Quintic Trajectory Planning Python Script

---
+++ needle_placement/scripts/trajectory_planning.py
@@ -0,0 +1,18 @@
+import numpy as np
+from quintic_trajectory import calculate_all_joint_trajectories
+
+# Define the inputs
+current_configuration = [0, 0, 0, 0, 0, 0, 0] # in radians
+desired_configuration = [1, 2, 3, 4, 5, 6, 7]
+duration = 5
+num_points = 6
+
+# Call the function
+trajectories = calculate_all_joint_trajectories(current_configuration, desired_configuration, duration, num_points)
+
+# Print the shape of the trajectories array (num_points,no_of_joints) # for panda, no_of_joints = 7
+print(trajectories.shape)
+
+# Print the trajectories array
+print(trajectories)
+
