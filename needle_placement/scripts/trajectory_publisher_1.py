c5de58f37a5407493170cb3daa5ebc9e6a101b9c
davidsosagomez <sosagomezdavid@gmail.com>
2023-06-20 14:50:19 +0200

    changes

--- needle_placement/scripts/trajectory_publisher_1.py
+++ needle_placement/scripts/trajectory_publisher_1.py
@@ -5,7 +5,7 @@ import rospy
 from std_msgs.msg import Float64MultiArray
 from sensor_msgs.msg import JointState
 import numpy as np
-from quintic_trajectory import calculate_all_joint_trajectories
+from rnm_trajectory_function import trajectory_generation
 
 current_joint_states = None
 desired_joint_states = None
@@ -18,28 +18,28 @@ def desired_goal_callback(msg):
     global desired_joint_states
     desired_joint_states = msg.position
 
-def publish_trajectory(duration, num_points):
+def publish_trajectory():
     # Create publisher
     pub = rospy.Publisher('/joint_position_example_controller_sim/joint_command', Float64MultiArray, queue_size=10)
 
     # Wait for the initial joint states and desired joint states to be received
     while current_joint_states is None or desired_joint_states is None:
-        rospy.sleep(0.1)
+        rospy.sleep(0.01)
 
     while not rospy.is_shutdown():
         # Wait for new desired joint states
         while desired_joint_states is None:
-            rospy.sleep(0.1)
+            rospy.sleep(0.01)
 
         # Store the current desired joint states to compare later
         current_desired_joint_states = desired_joint_states
 
         # Calculate trajectory
-        trajectories = calculate_all_joint_trajectories(current_joint_states, desired_joint_states, duration, num_points)
+        trajectories, duration, num_points = trajectory_generation(current_joint_states, desired_joint_states, duration, num_points)
 
         # Loop through the trajectory and publish each row
-        rate = rospy.Rate(1.0 / (duration / num_points))  # Publish frequency based on duration and num_points
-        # rate = rospy.Rate(1000)
+        #rate = rospy.Rate(1.0 / (duration / num_points))  # Publish frequency based on duration and num_points
+        rate = rospy.Rate(1000)
         for trajectory_point in trajectories:
             # Check if desired joint states have changed
             if desired_joint_states != current_desired_joint_states:
@@ -67,19 +67,19 @@ def main():
     # Subscribe to desired goal topic
     rospy.Subscriber('/desired_goal_states', JointState, desired_goal_callback)
 
-    if len(sys.argv) < 3:
-        print("Usage: python trajectory_publisher.py <duration> <num_points>")
-        sys.exit(1)
+    #if len(sys.argv) < 3:
+    #    print("Usage: python trajectory_publisher.py <duration> <num_points>")
+    #    sys.exit(1)
 
     # Extract duration and num_points from command-line arguments
-    duration = float(sys.argv[1])
-    num_points = int(sys.argv[2])
+    #duration = float(sys.argv[1])
+    #num_points = int(sys.argv[2])
 
     # Call the function to publish the trajectory
     try:
-        publish_trajectory(duration, num_points)
+        publish_trajectory()
     except rospy.ROSInterruptException:
         pass
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     main()
