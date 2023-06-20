c9195eb0e03a7463f1346f742635ac59bfbaa192
Omar Draidrya <omar.draidrya1@gmail.com>
2023-06-20 15:34:42 +0200

    update

--- scripts/trajectory_quintic_panda_limits.py
+++ scripts/trajectory_quintic_panda_limits.py
@@ -14,10 +14,9 @@ target_joint_angles = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
 # Interrupt flag for stopping the current motion and starting a new one
 interrupt_trajectory = False
 
-# Robot velocity, acceleration and jerk limits
-max_velocity = 2.5*0.0001  # rad/s
-max_acceleration = 25*0.00001  # rad/s²
-max_jerk = 0.00001  # rad/s³, value to be adjusted based on your robot's specifications
+# Robot velocity and acceleration limits
+max_velocity = 2.5*0.0001  # 2.5*0.00008 rad/s 
+max_acceleration = 25*0.00001  # 25.0*0.00008 rad/s² 
 
 # Quintic Coefficients Calculation
 def calculate_quintic_coefficients(t0, tf, q0, qf, v0, vf, a0, af):
@@ -54,44 +53,46 @@ def move_to_target(t0):
 
     # Calculate the necessary time to complete the motion (tf) for each joint
     tf_list = []
-    quintic_coefficients = []  # Store the coefficients for each joint
-
     for joint_index in range(len(joint_names)):
         q0 = initial_positions[joint_index]
         qf = target_joint_angles[joint_index]
 
         delta_q = abs(qf - q0)
 
-        # Assuming max acceleration and jerk, calculate time to reach max velocity
-        # and adjust it if the total motion delta_q is too small
-        tf_joint = max(np.sqrt(max_velocity/max_acceleration), np.cbrt(max_velocity/max_jerk))
-        tf_joint = min(tf_joint, np.sqrt(4 * delta_q / max_acceleration))
+        # Time needed to accelerate to max_velocity and decelerate to a stop
+        t_accel_decel = 2 * max_velocity / max_acceleration
+
+        if delta_q / max_velocity > t_accel_decel:
+            # The joint can reach max_velocity
+            # Time spent at max_velocity is the total time minus the acceleration and deceleration times
+            t_max_velocity = delta_q / max_velocity - t_accel_decel
+            tf_joint = t_accel_decel + t_max_velocity
+        else:
+            # The joint cannot reach max_velocity
+            # Use the kinematic equation: delta_q = 0.5*a*t^2 to solve for time
+            tf_joint = np.sqrt(4 * delta_q / max_acceleration)
 
         tf_list.append(tf_joint)
-
-        # Calculate quintic coefficients for each joint
-        conditions = {
-            'q0': q0,
-            'qf': qf,
-            'v0': 0.0,
-            'vf': 0.0,
-            'a0': 0.0,
-            'af': 0.0,
-        }
-        coeffs = calculate_quintic_coefficients(t0, tf_joint, **conditions)
-        quintic_coefficients.append(coeffs)
 
     # Use the maximum time required as tf
     tf = max(tf_list)
 
-    for t in np.arange(t0, tf, 0.1):
+    for t in np.arange(t0, tf, 0.01): # 0.01
         if rospy.is_shutdown() or interrupt_trajectory:
             break
 
         joint_positions = Float64MultiArray()
         for joint_index, joint_name in enumerate(joint_names):
-            joint_positions.data.append(polyval(t, quintic_coefficients[joint_index]))
-
+            conditions = {
+                'q0': initial_positions[joint_index],
+                'qf': target_joint_angles[joint_index],
+                'v0': 0.0,
+                'vf': 0.0,
+                'a0': 0.0,
+                'af': 0.0,
+            }
+            coeffs = calculate_quintic_coefficients(t0, tf, conditions['q0'], conditions['qf'], conditions['v0'], conditions['vf'], conditions['a0'], conditions['af'])
+            joint_positions.data.append(polyval(t, coeffs))
         pub.publish(joint_positions)
         rate.sleep()
 
@@ -105,7 +106,7 @@ if __name__ == '__main__':
     tf = None  # Will be calculated in move_to_target
 
     rospy.Subscriber('/joint_states', JointState, joint_states_callback)
-
+    
     pub = rospy.Publisher('/joint_position_example_controller_sim/joint_command', Float64MultiArray, queue_size=10)
     rate = rospy.Rate(100)  # 100 Hz
 
