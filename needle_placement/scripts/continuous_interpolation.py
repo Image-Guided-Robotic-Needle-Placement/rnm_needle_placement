c0383820ccd6f8579efe603c02d72fbef8bf4a71
davidsosagomez <sosagomezdavid@gmail.com>
2023-07-05 21:00:31 +0200

    please take a look selva

--- needle_placement/scripts/continuous_interpolation.py
+++ needle_placement/scripts/continuous_interpolation.py
@@ -1,6 +1,9 @@
 import numpy as np
+import rospy
 from quintic_functions import calculateSmoothRobotTrajectory
-from inverse_kinematics import inverse_kinematics
+from inverse_kinematics_function import inverse_kinematics
+from sensor_msgs.msg import JointState
+
 
 def interpolate(A_entry, A_ball, t):
     return (1.0 - t) * A_entry + t * A_ball
@@ -12,8 +15,20 @@ A_entry = np.array([[0.95663454, 0.28769805, 0.04560904, 0.283],
 A_ball = np.array([[0.95085546, 0.29124849, 0.10511047, 0.301],
                     [0.19840159, -0.83371212, 0.51532603, -0.139],
                     [0.2377198, -0.46914648, -0.85052388, 0.266]]).T.reshape(-1, 1)
+
 for t in np.linspace(0, 1, 5):
         current_joint_position = [0.33126478636661577, -0.8257679608411956, -1.0212689482370507, -2.6604452104735272, -0.21414480587762708, 1.936938619928904, -0.6195934342626068]
         interpolated_pose = interpolate(A_entry, A_ball, t)
         print("calculating inverse kinematics...")
         final_joint_angles = inverse_kinematics(current_joint_position, interpolated_pose)
+        current_joint_position = final_joint_angles.tolist()
+
+        joint_angles_msg = JointState()
+        joint_angles_msg.position = final_joint_angles
+        
+        pub.publish(joint_angles_msg)
+        rate.sleep()
+
+if __name__ == "__main__":
+    rospy.init_node('interpolation_publisher')
+    pub = rospy.Publisher('/interpolated_angles', JointState, queue_size=10)
