92656a84188d8a01826a27c637ee7cba650a5cf4
davidsosagomez <sosagomezdavid@gmail.com>
2023-07-09 13:31:35 +0200

    to go to initial configuration

--- needle_placement/scripts/initial_position_node.py
+++ needle_placement/scripts/initial_position_node.py
@@ -3,37 +3,36 @@
 import rospy
 from sensor_msgs.msg import JointState
 
-def initial_position_publisher():
+def joint_state_publisher():
     # Initialize the node
-    rospy.init_node('initial_position_node')
+    rospy.init_node('joint_state_publisher')
 
-    # Define the publisher, here it publishes to the /goal_states topic
-    # and the message type is JointState
+    # Create a publisher
+    # This will publish messages of type JointState to the /goal_states topic
+    # The queue_size parameter limits the amount of queued messages if any subscriber is not receiving them fast enough
     pub = rospy.Publisher('/goal_states', JointState, queue_size=10)
 
+    # Define joint names for Panda robot
+    joint_names = ["panda_joint1", "panda_joint2", "panda_joint3", "panda_joint4", "panda_joint5", "panda_joint6", "panda_joint7"]
+
     # Define the joint angles
-    initial_joint_angles = [-0.06345210742427591, -0.40445922335440626, 0.005874457170912318, -1.2728393697738647, 0.03285966468819975, 0.8744066302879874, -0.0010685555618854413]
+    joint_positions = [-0.06345210742427591, -0.40445922335440626, 0.005874457170912318, -1.2728393697738647, 0.03285966468819975, 0.8744066302879874, -0.0010685555618854413]
 
-    # Initialize the JointState message
-    joint_state_msg = JointState()
+    # Create a JointState message
+    msg = JointState()
+    msg.name = joint_names
+    msg.position = joint_positions
 
-    # Populate the JointState message
-    # In this case, I assume the robot has 7 joints named 'joint1' to 'joint7'.
-    joint_state_msg.name = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6', 'joint7']
-    joint_state_msg.position = initial_joint_angles
+    # Sleep to ensure the publisher is set up before we start publishing.
+    rospy.sleep(1)
 
-    # Sleep for a bit to make sure the publisher is connected
-    rospy.sleep()
+    # Publish the message once
+    pub.publish(msg)
 
-    # Publish the joint angles
-    pub.publish(joint_state_msg)
-    
-    # Print the message
-    rospy.loginfo("Published joint goal states.")
+    rospy.loginfo("Published goal joint state to /goal_states")
 
-    if __name__ == '__main__':
-        try:
-            initial_position_publisher()
-        except rospy.ROSInterruptException:
-            pass
-
+if __name__ == '__main__':
+    try:
+        joint_state_publisher()
+    except rospy.ROSInterruptException:
+        pass
