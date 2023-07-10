6e74ba44d5b035752ae858e8c7ec5e6996f72fed
davidsosagomez <sosagomezdavid@gmail.com>
2023-07-10 14:31:36 +0200

    bool to set to true

--- needle_placement/scripts/continuous_interpolation.py
+++ needle_placement/scripts/continuous_interpolation.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import rospy
-from std_msgs.msg import Float64MultiArray 
+from std_msgs.msg import Float64MultiArray, Bool
 from quintic_functions import calculateSmoothRobotTrajectory
 from inverse_kinematics_function import inverse_kinematics
 from sensor_msgs.msg import JointState
@@ -17,10 +17,12 @@ class InterpolationPublisher:
         # Subscribe to A_entry and A_ball topics
         rospy.Subscriber('/A_entry', Float64MultiArray, self.entry_callback)
         rospy.Subscriber('/A_ball', Float64MultiArray, self.ball_callback)
+        rospy.Subscriber('/reached', Bool, self.reached_callback)
         
         self.A_entry = None
         self.A_ball = None
         self.current_joint_position = None
+        self.reached = False
 
         self.rate = rospy.Rate(1)
 
@@ -33,12 +35,15 @@ class InterpolationPublisher:
 
     def ball_callback(self, msg):
         self.A_ball = np.array(msg.data).T.reshape(-1, 1)
+
+    def reached_callback(self, msg):
+        self.reached = msg.data
         
     def interpolate(self, A_entry, A_ball, t):
         return (1.0 - t) * A_entry + t * A_ball
 
     def run(self):
-        while not rospy.is_shutdown() and (self.current_joint_position is None or self.A_entry is None or self.A_ball is None):
+        while not rospy.is_shutdown() and (self.current_joint_position is None or self.A_entry is None or self.A_ball is None or self.reached is False):
             rospy.sleep(0.1)
 
         for t in np.linspace(0, 1, 10):
